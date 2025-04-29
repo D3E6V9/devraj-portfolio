30db0689e4806b22a5f75ecfe505d06a5f4d73c9
Devraj Joshi <devraj.np369@gmail.com>
2025-04-29 08:32:16 +0545

    Add SEO optimizations including meta tags, sitemap, and robots.txt

--- portfolio/portfolio/main_app/urls.py
+++ portfolio/portfolio/main_app/urls.py
@@ -1,5 +1,8 @@
 # main_app/urls.py
 from django.urls import path
+from django.views.static import serve
+import os
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -8,5 +11,15 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
     path('threesixnine/', views.threesixnine, name='threesixnine'),
     path('threesixninepix/', views.threesixninepix, name='threesixninepix'),
-    path('export-messages/', views.export_messages, name='export_messages'),  # New URL for exporting messages
+    path('export-messages/', views.export_messages, name='export_messages'),  # URL for exporting messages
+    
+    # SEO-related paths
+    path('robots.txt', serve, {
+        'path': 'robots.txt',
+        'document_root': os.path.join(settings.BASE_DIR, 'main_app/static')
+    }),
+    path('sitemap.xml', serve, {
+        'path': 'sitemap.xml',
+        'document_root': os.path.join(settings.BASE_DIR, 'main_app/static')
+    }),
 ]
